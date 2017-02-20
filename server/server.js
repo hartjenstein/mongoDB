06b63e14b88a4e99475d8879d21835a635f5f5fa
Nick Hartjenstein <nick@iMac.local>
2017-02-20 13:54:50 +0100

    *

--- server/server.js
+++ server/server.js
@@ -1,59 +1,56 @@
-// server file is just for our routes now
-let express = require('express');
-//body-parser converts JSON into an object 
-let bodyParser = require('body-parser');
-// destructing object - ES6 (saving the mongoose return value in the variable)
-const { ObjectID } = require('mongodb');
-let { mongoose } = require('./db/mongoose');
-let { Todo } = require('./models/todo');
-let { User } = require('./models/user');
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-let app = express();
-//crud - create read update delete
-const port = process.env.PORT || 3000; 
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-   let todo = new Todo({
-       text: req.body.text
-    });
-//test
-    todo.save().then((doc) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
     res.send(doc);
-    }, (e) => {
+  }, (e) => {
     res.status(400).send(e);
-    });
+  });
 });
-// sending todos as an array (res.send(todos)) would not allow us to add any properties
-// with ES6 we can send it as an object: res.send({todos})
+
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({todos});
-    }, (e) => {
-        res.status(400).send(e);
-    });
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-// GET / todos / 12345454
 app.get('/todos/:id', (req, res) => {
-   // res.send(req.params.id);
-    let id = req.params.id;
-    //check if the id is valid
-    if (!ObjectID.isValid(id)) {
-       return res.status(404).send();
-    }   
-    Todo.findById(id).then((todo) => {
-            if(!todo) {
-                return res.status(404).send();
-            }
-            res.status(200).send({todo});
-        }).catch((e) => {
-            res.status(400).send();
-        });   
-    }); 
-app.listen(port, () => {
-    console.log(`started on port ${port}`);
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
-// ES6 syntax
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
 module.exports = {app};
---
+++ server/server2.js
@@ -0,0 +1,59 @@
+// server file is just for our routes now
+let express = require('express');
+//body-parser converts JSON into an object 
+let bodyParser = require('body-parser');
+// destructing object - ES6 (saving the mongoose return value in the variable)
+const { ObjectID } = require('mongodb');
+let { mongoose } = require('./db/mongoose');
+let { Todo } = require('./models/todo');
+let { User } = require('./models/user');
+
+let app = express();
+//crud - create read update delete
+const port = process.env.PORT || 3000; 
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+   let todo = new Todo({
+       text: req.body.text
+    });
+//test
+    todo.save().then((doc) => {
+    res.send(doc);
+    }, (e) => {
+    res.status(400).send(e);
+    });
+});
+// sending todos as an array (res.send(todos)) would not allow us to add any properties
+// with ES6 we can send it as an object: res.send({todos})
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+// GET / todos / 12345454
+app.get('/todos/:id', (req, res) => {
+   // res.send(req.params.id);
+    let id = req.params.id;
+    //check if the id is valid
+    if (!ObjectID.isValid(id)) {
+       return res.status(404).send();
+    }   
+    Todo.findById(id).then((todo) => {
+            if(!todo) {
+                return res.status(404).send();
+            }
+            res.status(200).send({todo});
+        }).catch((e) => {
+            res.status(400).send();
+        });   
+    }); 
+app.listen(port, () => {
+    console.log(`started on port ${port}`);
+});
+
+// ES6 syntax
+module.exports = {app};
