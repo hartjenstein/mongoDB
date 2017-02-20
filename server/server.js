146cf51f8e6833ed7884f0f4629726fa3e4fd985
nicolas hartjenstein <hartjenstein@Hartjensteins.local>
2017-02-20 22:05:43 +0100

    added delete to postman - heroku - mongo up and running

--- server/server.js
+++ server/server.js
@@ -48,6 +48,22 @@ app.get('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+app.delete('/todos/:id', (req, res) => {
+  //get id
+  var id = req.params.id;
+  //validate id -> not valid - 404
+   if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((doc) => {
+    if (!doc) {
+      return res.status(404).send();
+    }
+    res.status(200).send(doc);
+    }).catch((e) => {
+    res.status(400).send();
+    });
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
